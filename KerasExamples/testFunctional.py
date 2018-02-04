af73a97acd1b103ce94660abcd87337bfd2d17f9
umroot <qdsclove@163.com>
2018-02-04 17:29:23 -0500

    learning keras 0

---
+++ KerasExamples/testFunctional.py
@@ -0,0 +1,5 @@
+from keras.layers import Input, Dense
+from keras.models import Model
+
+# This returns a tensor
+inputs = Input(shape=(784,))
--- testKeras.py
+++ testKeras.py
@@ -1,10 +1,27 @@
+import numpy as np
+
 from keras.models import Sequential
-model = Sequential()
 from keras.layers import Dense, Activation
 
-model.add(Dense(units=64, input_dim=100))
-model.add(Activation('relu'))
-model.add(Dense(units=10))
-model.add(Activation('softmax'))
+a = np.array([[1, 2], [3, 4]])
+sum0 = np.sum(a, axis=0)
+sum1 = np.sum(a, axis=1)
 
-model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
+print (sum0)
+print (sum1)
+
+#######
+
+model = Sequential()
+model.add(Dense(32, activation='relu', input_dim=100))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer="rmsprop",
+              loss="binary_crossentropy",
+              metrics=['accuracy'])
+
+# Generate dummy data
+data = np.random.random((1000, 100))
+labels = np.random.randint(2, size=(1000, 1))
+
+# Train the model
+model.fit(data, labels, epochs=10, batch_size=32)
